d02891cccb3f97288419795158960d4861f10c50
fpendii <144331827+fpendii@users.noreply.github.com>
2025-01-06 19:28:25 +0800

    update view servis

--- app/Http/Controllers/Admin/LaporanServisController.php
+++ app/Http/Controllers/Admin/LaporanServisController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LaporanServisController extends Controller
 {
     public function servis()
     {
-        return view('admin.laporan-keuangan.servis.index');
+        $transaksi = DB::table('pesanan_servis')->get();
+
+        return view('admin.laporan-keuangan.servis.index',compact('transaksi'));
     }
 }
--- resources/views/admin/laporan-keuangan/servis/index.blade.php
+++ resources/views/admin/laporan-keuangan/servis/index.blade.php
@@ -10,27 +10,65 @@
 
 @section('content')
     @include('components.template-admin.navbar-laporan-keuangan')
+    @include('components.alert.alert')
     <div class="card">
+
 
         <div class="row my-4">
             <!-- Orders Overview -->
             <div class="col-lg-4 col-md-6 order-1 order-lg-2">
                 <div class="card h-100">
                     <div class="card-header pb-0">
-                        <h6>Orders Overview</h6>
-                        <p class="text-sm">
-                            <i class="fa fa-arrow-up text-success" aria-hidden="true"></i>
-                            <span class="font-weight-bold">Rp. 250.000</span> bulan ini
-                        </p>
+                        <h6>Uang Kas</h6>
+                        {{-- <p class="text-sm">
+                            <i class="fa  {{ $total_pemasukan > 0 ? 'fa-arrow-up text-success' : 'fa-arrow-down text-danger' }}" aria-hidden="true"></i>
+                            <span class="font-weight-bold">Rp. {{ number_format($total_pemasukan) }}</span>
+                        </p> --}}
                     </div>
                     <div class="card-body p-3">
                         <div class="d-flex justify-content-between align-items-center">
-                            <a href="{{ url('admin/order/'.$page.'/tambah') }}" class="btn btn-primary btn-sm">
+
+                            <a href="{{ url('admin/order/' . $page . '/tambah') }}" class="btn btn-primary btn-sm">
                                 <i class="fa fa-plus"></i> Tambah
                             </a>
                         </div>
                     </div>
+                    <div class="card h-100">
+                        <div class="card-header pb-0">
+                            <h6>Pengeluaran Bulan ini</h6>
+
+                        </div>
+                        {{-- <div class="card-body p-3">
+                            @php
+                                $no = 1;
+                            @endphp
+                            @foreach ($transaksi as $item)
+                                <div class="timeline timeline-one-side">
+                                    <div class="timeline-block mb-3">
+                                        <span class="timeline-step">
+                                            <i
+                                                class="ni ni-bell-55 {{ $no % 2 === 0 ? 'text-success' : 'text-danger' }} text-gradient"></i>
+                                        </span>
+                                        <div class="timeline-content">
+                                            <h6 class="text-dark text-sm font-weight-bold mb-0">
+                                                {{ number_format($item->jumlah) }}, {{ $item->keterangan }}
+                                            </h6>
+                                            <p class="text-secondary font-weight-bold text-xs mt-1 mb-0">
+                                                {{ $item->tanggal_transaksi }}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                                @php
+                                    $no++; // Increment counter
+                                @endphp
+                            @endforeach
+
+
+                        </div> --}}
+                    </div>
                 </div>
+
             </div>
             <!-- Printing Orders Table -->
             <div class="col-lg-8 col-md-6 order-2 order-lg-1">
@@ -51,7 +89,7 @@
                                     </a>
                                     <ul class="dropdown-menu px-2 py-3 ms-sm-n4 ms-n5" aria-labelledby="dropdownTable">
                                         <li><a class="dropdown-item border-radius-md"
-                                                href="#">Lihat semua</a></li>
+                                                href="{{ url('admin/laporan-keuangan/printing/all') }}">Lihat semua</a></li>
                                     </ul>
                                 </div>
                             </div>
@@ -63,76 +101,103 @@
                                 <!-- Table -->
                                 <thead>
                                     <tr>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama</th>
-                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Jenis Kerusakan</th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Harga Modal</th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Harga Jual</th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Laba/Untung</th>
-                                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Aksi</th>
+                                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Nama
+                                        </th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                            Jenis Servis</th>
+                                        <th
+                                            class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
+                                            Harga Modal</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Harga Jual</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Laba</th>
+                                        <th
+                                            class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                            Aksi</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>
-                                            <div class="d-flex px-2 py-1">
-                                                <div class="d-flex flex-column justify-content-center">
-                                                    <h6 class="mb-0 text-sm">Andi</h6>
+                                    @foreach ($transaksi as $item)
+                                        <tr>
+                                            <td>
+                                                <div class="d-flex px-2 py-1">
+                                                    <div class="d-flex flex-column justify-content-center">
+                                                        <h6 class="mb-0 text-sm">{{ $item->nama_pelanggan }}</h6>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </td>
-                                        <td class="w-30 text-wrap">
-                                            <div class="avatar-group mt-2">
-                                                <small>M, L</small>
-                                            </div>
-                                        </td>
-                                        <td class="w-30 text-wrap">
-                                            <div class="avatar-group mt-2">
-                                                <small>M, L</small>
-                                            </div>
-                                        </td>
-                                        <td class="w-30 text-wrap">
-                                            <div class="avatar-group mt-2">
-                                                <small>M, L</small>
-                                            </div>
-                                        </td>
-                                        <td class="align-middle text-center text-sm">
-                                            <span class="text-xs font-weight-bold"> Rp. 50.000 </span>
-                                        </td>
-                                        <td class="align-middle text-center">
-                                            <!-- Tombol Edit -->
-                                            <a href="#" class="btn btn-warning btn-sm">
-                                                <i class="fa fa-pencil-alt"> Edit</i>
-                                            </a>
-                                            <!-- Tombol Hapus -->
-                                            <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
-                                                data-bs-target="#deleteModal1">
-                                                <i class="fa fa-trash"></i> Hapus
-                                            </button>
+                                            </td>
+                                            <td class="w-30 text-wrap">
+                                                <div class="avatar-group mt-2">
+                                                    <small>{{ $item->jenis_servis }}</small>
+                                                </div>
+                                            </td>
+                                            <td class="w-30 text-wrap">
+                                                <div class="avatar-group mt-2">
+                                                    <small>{{ $item->harga_modal }}</small>
+                                                </div>
+                                            </td>
+                                            <td class="align-middle text-center text-sm">
+                                                <span class="text-xs font-weight-bold">
+                                                    {{ number_format($item->harga_jual) }} </span>
+                                            </td>
+                                            <td class="align-middle text-center text-sm">
+                                                <span class="text-xs font-weight-bold"> Rp.
+                                                    {{ number_format($item->laba) }} </span>
+                                            </td>
+                                            <td class="align-middle text-center">
+                                                <!-- Tombol Edit -->
+                                                <a href="{{ url('admin/order/printing/edit/' . $item->id_pesanan_servis) }}"
+                                                    class="btn btn-warning btn-sm">
+                                                    <i class="fa fa-pencil-alt"> Edit</i>
+                                                </a>
+                                                <!-- Tombol Hapus -->
+                                                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal"
+                                                    data-bs-target="#deleteModal{{ $item->id_pesanan_servis }}">
+                                                    <i class="fa fa-trash"></i> Hapus
+                                                </button>
 
-                                            <!-- Modal -->
-                                            <div class="modal fade" id="deleteModal1" tabindex="-1"
-                                                aria-labelledby="deleteModalLabel1"
-                                                aria-hidden="true">
-                                                <div class="modal-dialog">
-                                                    <div class="modal-content">
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title" id="deleteModalLabel1">Konfirmasi Hapus</h5>
-                                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            Apakah Anda yakin ingin menghapus data ini?
-                                                        </div>
-                                                        <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                                            <button type="button" class="btn btn-danger">Hapus</button>
+                                                <!-- Modal -->
+                                                <div class="modal fade" id="deleteModal{{ $item->id_pesanan_servis }}"
+                                                    tabindex="-1"
+                                                    aria-labelledby="deleteModalLabel{{ $item->id_pesanan_servis }}"
+                                                    aria-hidden="true">
+                                                    <div class="modal-dialog">
+                                                        <div class="modal-content">
+                                                            <div class="modal-header">
+                                                                <h5 class="modal-title"
+                                                                    id="deleteModalLabel{{ $item->id_pesanan_servis }}">
+                                                                    Konfirmasi
+                                                                    Hapus</h5>
+                                                                <button type="button" class="btn-close"
+                                                                    data-bs-dismiss="modal" aria-label="Close"></button>
+                                                            </div>
+                                                            <div class="modal-body">
+                                                                Apakah Anda yakin ingin menghapus data ini?
+                                                            </div>
+                                                            <div class="modal-footer">
+                                                                <button type="button" class="btn btn-secondary"
+                                                                    data-bs-dismiss="modal">Batal</button>
+                                                                <form
+                                                                    action="{{ url('admin/order/printing/hapus/' . $item->id_pesanan_servis) }}"
+                                                                    method="POST" class="d-inline">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                    <button type="submit"
+                                                                        class="btn btn-danger">Hapus</button>
+                                                                </form>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                        </td>
-                                    </tr>
-
+                                            </td>
+                                        </tr>
+                                    @endforeach
                                 </tbody>
+
                             </table>
                         </div>
                     </div>
