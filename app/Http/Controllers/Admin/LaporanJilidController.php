6cf32ec203c2e45a823c65c4a7c4e8c1ccb5777f
fpendii <144331827+fpendii@users.noreply.github.com>
2025-01-14 13:09:03 +0800

    update fitur delete jilid

--- app/Http/Controllers/Admin/LaporanJilidController.php
+++ app/Http/Controllers/Admin/LaporanJilidController.php
@@ -27,4 +27,36 @@ class LaporanJilidController extends Controller
 
         return view('admin.laporan-keuangan.jilid.edit', compact('transaksi'));
     }
+
+    public function hapus($id){
+        DB::table('pesanan_jilid')->where('id_pesanan_jilid', $id)->delete();
+
+        return redirect()->to('admin/laporan-keuangan/jilid')->with('success', 'Data berhasil dihapus');
+    }
+
+    public function simpan(Request $request){
+        $request->validate([
+            'nama_pelanggan' => 'required',
+            'jenis_jilid' => 'required',
+            'jumlah' => 'required|numeric|min:1',
+            'total_harga' => 'required|numeric|min:1',
+            'nama_dokument' => 'required_if:mode,pengeluaran|file|mimes:pdf,doc,docx,xls,xlsx,ppt,pptx',
+        ], [
+            'nama_dokumen.required_if' => 'Dokumen harus diisi jika mode adalah pengeluaran',
+            'nama_dokumen.mimes' => 'Dokumen harus berupa file PDF, DOC, DOCX, XLS, XLSX, PPT, PPTX',
+        ]);
+
+        $total_harga = str_replace('.', '', $request->total_harga);
+
+        DB::table('pesanan_jilid')->insert([
+            'nama_pelanggan' => $request->nama_pelanggan,
+            'jenis_jilid' => $request->jenis_jilid,
+            'jumlah' => $request->jumlah,
+            'total_harga' => $total_harga,
+            'dokumen' =>  $request->file('nama_dokument')->getClientOriginalName(),
+        ]);
+
+        return redirect()->to('admin/laporan-keuangan/jilid')->with('success', 'Data berhasil disimpan');
+    }
+
 }
