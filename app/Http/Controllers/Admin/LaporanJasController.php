6148e28d746e0d7c03c80e2d9c67e32ff497a279
fpendii <144331827+fpendii@users.noreply.github.com>
2025-01-16 22:45:17 +0800

    update view tambah jas

--- app/Http/Controllers/Admin/LaporanJasController.php
+++ app/Http/Controllers/Admin/LaporanJasController.php
@@ -17,4 +17,29 @@ class LaporanJasController extends Controller
     public function tambah(){
         return view('admin.laporan-keuangan.jas.tambah');
     }
+
+    public function simpan(Request $request){
+        
+        $request->validate(
+            [
+                'id_pesanan_jas' => 'required',
+                'id_user' => 'required',
+                'id_jas' => 'required',
+                'jumlah' => 'required',
+                'status' => 'required',
+            ]
+            );
+
+        DB::table('pesanan_jas')->insert([
+            'id_pesanan_jas' => $request->id_pesanan_jas,
+            'id_user' => $request->id_user,
+            'id_jas' => $request->id_jas,
+            'jumlah' => $request->jumlah,
+            'status' => $request->status,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return redirect()->to('admin/laporan-keuangan/jas')->with('success', 'Data berhasil disimpan');
+    }
 }
