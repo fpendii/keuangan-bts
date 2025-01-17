3d0d6fd4b39113176804aefb9dd83fcc94ff0138
fpendii <144331827+fpendii@users.noreply.github.com>
2025-01-17 15:43:24 +0800

    update fitur hapus jas

--- app/Http/Controllers/Admin/LaporanJasController.php
+++ app/Http/Controllers/Admin/LaporanJasController.php
@@ -54,4 +54,45 @@ class LaporanJasController extends Controller
 
         return redirect()->to('admin/laporan-keuangan/jas')->with('success', 'Data berhasil disimpan');
     }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate(
+            [
+                'nama_pelanggan' => 'required',
+                'ukuran_jas' => 'required',
+                'jumlah' => 'required|numeric|min:1',
+                'total_harga' => 'required|numeric|min:1',
+            ],
+            [
+                'nama_pelanggan.required' => 'Nama pelanggan harus diisi.',
+                'ukuran_jas.required' => 'Ukuran jas harus diisi.',
+                'jumlah.required' => 'Jumlah lembar harus diisi.',
+                'jumlah.numeric' => 'Jumlah lembar harus berupa angka.',
+                'jumlah.min' => 'Jumlah lembar minimal 1.',
+                'total_harga.required' => 'Total harga harus diisi.',
+                'total_harga.numeric' => 'Total harga harus berupa angka.',
+                'total_harga.min' => 'Total harga minimal 1.',
+            ]
+        );
+
+        $total_harga = str_replace('.', '', $request->total_harga);
+
+        DB::table('pesanan_jas')->where('id_pesanan_jas', $id)->update([
+            'nama_pelanggan' => $request->nama_pelanggan,
+            'ukuran_jas' => $request->ukuran_jas,
+            'jumlah' => $request->jumlah,
+            'total_harga' => $total_harga,
+            'updated_at' => now(),
+        ]);
+
+        return redirect()->to('admin/laporan-keuangan/jas')->with('success', 'Data berhasil diubah');
+    }
+
+    public function hapus($id)
+    {
+        DB::table('pesanan_jas')->where('id_pesanan_jas', $id)->delete();
+
+        return redirect()->to('admin/laporan-keuangan/jas')->with('success', 'Data berhasil dihapus');
+    }
 }
